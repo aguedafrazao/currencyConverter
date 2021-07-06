cf7e92812e97d5944fbeb0586ae5c66a94e9339e
aguedafrazao <aguedaaraujo13@gmail.com>
2021-07-06 10:01:32 -0300

    fixed

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import './Converter.css'
 
 export default class Converter extends Component {
-    // acessar os atributos
     constructor(props){
         super(props);
 
@@ -10,7 +9,6 @@ export default class Converter extends Component {
             coinOneValue: "",
             coinTwoValue: 0,
         }
-        // criando um bind 
         this.converter = this.converter.bind(this);
     }
 
@@ -32,7 +30,6 @@ export default class Converter extends Component {
         return (
             <div className="converter">
                 <h2>{this.props.coinOne} for {this.props.coinTwo}</h2>
-                {/* onChange vai disparar um evento, e o target vai pegar o valor do input e passar p minha var */}
                 <input type="text" onChange={(event)=>{this.setState({coinOneValue:event.target.value})}}></input>
                 <input type="button" value="Convert" onClick = {this.converter}></input>
                 <h2>Converted= {this.state.coinTwoValue}</h2>
